88c3ab103b40d839700dedc5c8afe95fedf47b8c
Alexander Blake-Martin <alexbm173@gmail.com>
2026-01-10 20:59:09 +0000

    Add Sphinx documentation scaffolding
    Introduced Sphinx documentation support by adding Makefile, make.bat, and initial configuration in conf.py. Created a starter index.rst file with basic structure and instructions for adding content.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,5 +1,28 @@
-project = "C1 Research Computing and Software Development"
-author = "Alex Blake Martín"
-copyright = "2026, Alex Blake Martín"
-version = "0.1"
-master_doc = "index"
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'C1 Research Computing and Software Development'
+copyright = '2026, Alex Blake Martín'
+author = 'Alex Blake Martín'
+release = '0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
---
+++ docs/source/index.rst
@@ -0,0 +1,17 @@
+.. C1 Research Computing and Software Development documentation master file, created by
+   sphinx-quickstart on Sat Jan 10 20:57:36 2026.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+C1 Research Computing and Software Development documentation
+============================================================
+
+Add your content using ``reStructuredText`` syntax. See the
+`reStructuredText <https://www.sphinx-doc.org/en/master/usage/restructuredtext/index.html>`_
+documentation for details.
+
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
